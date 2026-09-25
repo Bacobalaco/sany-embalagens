31e7c567b08221c4f197f088b52f6376ac445cb5
Bacobalaco <davidjoes15@gmail.com>
2026-09-25 10:42:01 -0300

    feat: layout da SANY

---
+++ app/layout.jsx
@@ -0,0 +1,4 @@
+export const metadata = { title: 'SANY Embalagens', description: 'Sistema financeiro da SANY Embalagens' };
+export default function RootLayout({ children }) {
+  return <html lang="pt-BR"><body>{children}</body></html>;
+}
